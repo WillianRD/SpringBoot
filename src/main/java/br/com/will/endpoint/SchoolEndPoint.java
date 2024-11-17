0b17da49fcf8a2a7b0ad4eebd07e6ed7aacb059a
WillianRD <willianrodriguesdos1@gmail.com>
2024-11-17 08:57:26 -0300

    Three commit Spring Add School

---
+++ src/main/java/br/com/will/endpoint/SchoolEndPoint.java
@@ -0,0 +1,20 @@
+package br.com.will.endpoint;
+
+import br.com.will.model.School;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
+import static java.util.Arrays.asList;
+
+@RestController
+@RequestMapping()
+public class SchoolEndPoint {
+    @RequestMapping(method= RequestMethod.GET,path = "/school")
+    public List<School>  schools (){
+        return asList(new School(1,"Faculdade Anhanguera Anchieta","Rua Atlantica","(11) 98910-0652","anhangueraSbc@anhanguera.com",
+                "https://login.kroton.com.br/AccountAluno/Login?client_id=169104&response_type=code&referrer=alunodigital.anhanguera.com",1000,40),
+        new School(2,"USP","São Paulo","11 4066-2476","universidadeUsp@usp.com",
+                "www.usp.com.br",5000,120));
+    }
+}
---
+++ src/main/java/br/com/will/model/School.java
@@ -0,0 +1,47 @@
+package br.com.will.model;
+
+public class School {
+    private long id;
+    private String nameSchool, address, cellPhone, email, webSite;
+    private int amountStudents, amountRoom;
+
+    public School(long id, String nameSchool, String address, String cellPhone, String email, String webSite,
+                  int amountStudents, int amountRoom) {
+        this.id = id;
+        this.nameSchool = nameSchool;
+        this.address = address;
+        this.cellPhone = cellPhone;
+        this.email = email;
+        this.webSite = webSite;
+        this.amountStudents = amountStudents;
+        this.amountRoom = amountRoom;
+    }
+
+    public String getNameSchool() {
+        return nameSchool;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getCellPhone() {
+        return cellPhone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getWebSite() {
+        return webSite;
+    }
+
+    public int getAmountStudents(){
+        return amountStudents;
+    }
+
+    public int getAmountRoom(){
+        return amountRoom;
+    }
+}
