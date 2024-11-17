d2110ba6f30c0a428f38afa14015b6c483e9ba8a
WillianRD <willianrodriguesdos1@gmail.com>
2024-11-17 11:54:29 -0300

    Five Commit Spring Boot

--- src/main/java/br/com/will/endpoint/AnimeEndPoint.java
+++ src/main/java/br/com/will/endpoint/AnimeEndPoint.java
@@ -11,8 +11,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
 import java.time.LocalDateTime;
 import java.util.List;
+
 import static java.util.Arrays.asList;
 
 @RestController
@@ -21,12 +23,18 @@ import static java.util.Arrays.asList;
 //@AllArgsConstructor // Criar um construtor com todos os atributos que a classe tem
 //@RequiredArgsConstructor // Cria contrutor com os campos que são finais
 public class AnimeEndPoint {
-    @RequestMapping(method = RequestMethod.GET, path = "/anime")
-//    private DateUtil dateUtil;
+//    private final DateUtil dateUtil;
 
-//    @GetMapping(path = "/list") // Realiza a mesma coisa do @RequestMapping, mas de uma forma que tenha menos códigos
-    public List<Anime> animes(){
+    @GetMapping(path = "/anime") // Realiza a mesma coisa do @RequestMapping, mas de uma forma que tenha menos códigos
+    public List<Anime> animes() {
 //        log.info(dateUtil.formatLocalDateTimeToDatabaseStyle(LocalDateTime.now()));
-        return asList((new Anime("Naruto", 500)));
+        return asList((new Anime("Naruto", 500))); //    @GetMapping(path = "/list") // Realiza a mesma coisa do @RequestMapping, mas de uma forma que tenha menos códigos@GetMapping(path = "/anime") // Realiza a mesma coisa do @RequestMapping, mas de uma forma que tenha menos códigos
     }
-}
+
+    @GetMapping(path = "/animeeps")
+        public List<Anime> animeList () {
+//        log.info(dateUtil.formatLocalDateTimeToDatabaseStyle(LocalDateTime.now()));
+            return asList((new Anime("One Piece", 500))); //    @GetMapping(path = "/list") // Realiza a mesma coisa do @RequestMapping, mas de uma forma que tenha menos códigos
+
+        }
+    }
