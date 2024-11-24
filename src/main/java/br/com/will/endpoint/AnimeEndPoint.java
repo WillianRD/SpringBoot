dff295d3cf5334ccee43a5e5b5653a715e94e8f8
WillianRD <willianrodriguesdos1@gmail.com>
2024-11-24 15:26:51 -0300

    Anime EndPoint

--- src/main/java/br/com/will/endpoint/AnimeEndPoint.java
+++ src/main/java/br/com/will/endpoint/AnimeEndPoint.java
@@ -1,15 +1,15 @@
 package br.com.will.endpoint;
 
 import br.com.will.model.Anime;
+import br.com.will.services.AnimeServices;
 import br.com.will.util.DateUtil;
-import lombok.AllArgsConstructor;
+
+import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDateTime;
@@ -17,24 +17,27 @@ import java.util.List;
 
 import static java.util.Arrays.asList;
 
-@RestController
-//@RequestMapping("/anime")
-//@Log4j2
 //@AllArgsConstructor // Criar um construtor com todos os atributos que a classe tem
-//@RequiredArgsConstructor // Cria contrutor com os campos que são finais
-public class AnimeEndPoint {
-//    private final DateUtil dateUtil;
+//@Data // Gera Getters/setter/equals/hashCode/toString
 
-    @GetMapping(path = "/anime") // Realiza a mesma coisa do @RequestMapping, mas de uma forma que tenha menos códigos
+@RequestMapping("animes")
+@Log4j2
+@RequiredArgsConstructor // Cria contrutor com os campos que são finais
+@RestController
+
+public class AnimeEndPoint {
+    private AnimeServices animeServices;
+    @GetMapping // Realiza a mesma coisa do @RequestMapping, mas de uma forma que tenha menos códigos
     public List<Anime> animes() {
 //        log.info(dateUtil.formatLocalDateTimeToDatabaseStyle(LocalDateTime.now()));
-        return asList((new Anime("Naruto", 500))); //    @GetMapping(path = "/list") // Realiza a mesma coisa do @RequestMapping, mas de uma forma que tenha menos códigos@GetMapping(path = "/anime") // Realiza a mesma coisa do @RequestMapping, mas de uma forma que tenha menos códigos
+        return asList((new Anime(1, "Naruto", 500))); //    @GetMapping(path = "/list") // Realiza a mesma coisa do @RequestMapping, mas de uma forma que tenha menos códigos@GetMapping(path = "/anime") // Realiza a mesma coisa do @RequestMapping, mas de uma forma que tenha menos códigos
     }
 
-    @GetMapping(path = "/animeeps")
-        public List<Anime> animeList () {
+    @GetMapping(path = "/anime2")
+    public List<Anime> animeList() {
 //        log.info(dateUtil.formatLocalDateTimeToDatabaseStyle(LocalDateTime.now()));
-            return asList((new Anime("One Piece", 500))); //    @GetMapping(path = "/list") // Realiza a mesma coisa do @RequestMapping, mas de uma forma que tenha menos códigos
+        return animeServices.listAll();    //   @GetMapping(path = "/list") // Realiza a mesma coisa do @RequestMapping, mas de uma forma que tenha menos códigos
 
-        }
     }
+}
+
--- src/main/java/br/com/will/model/Anime.java
+++ src/main/java/br/com/will/model/Anime.java
@@ -1,19 +1,13 @@
 package br.com.will.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data // Gerando getters/setters/equals/hashCode/toString
+@AllArgsConstructor // Criando um construtor com todos os atributos
 public class Anime {
+    private long id;
     private String nameAnime;
     private int eps;
 
-    public Anime(String nameAnime, int eps){
-        this.nameAnime = nameAnime;
-        this.eps = eps;
-    }
-
-    public String getNameAnime(){
-        return nameAnime;
-    }
-
-    public int getEps(){
-        return eps;
-    }
 }
